3226bb57405a9d1121524d20dcd6b8e565327c58
nihalaftab <240034.cs@rmkec.ac.in>
2026-06-05 20:18:06 +0530

    Implement string combinations generator
    Added a program to generate all combinations of a string.

--- combinations.java
+++ combinations.java
@@ -1,1 +1,26 @@
-one
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your string: ");
+        String s = sc.next();
+
+        int n = s.length();
+
+        for (int i = 1; i < (1 << n); i++) {
+            StringBuilder ans = new StringBuilder();
+
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    ans.append(s.charAt(j));
+                }
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
